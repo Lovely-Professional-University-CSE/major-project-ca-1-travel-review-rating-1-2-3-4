bac566765a798bc826db49d2daa9048d6a012d38
pull5 <35608695+pull5@users.noreply.github.com>
2019-10-30 13:20:43 +0530

    Add files via upload
    added stemmer
    punctuation removal
    count vectorizer

---
+++ project test.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from nltk.corpus import stopwords
+stop_words=set(stopwords.words("english"))
+from sklearn.feature_extraction.text import CountVectorizer
+vector=CountVectorizer()
+review=pd.read_csv("G:/ww.csv",encoding = 'unicode_escape')
+table=str.maketrans("!@#$%^&*().,",12*" ")
+data=[]
+answer=[]
+
+for index, row in review.iterrows():
+    data.append(row["Review"])
+    answer.append(row["Rating"])
+print(answer)
+for  i in range(len(data)):
+    data[i]=data[i].translate(table)
+vector.fit(data)
+from nltk.stem.snowball import SnowballStemmer  
+dat=[]
+stemmer=SnowballStemmer("english")
+for i in (data):
+    if(stemmer.stem(i) not in stop_words):
+        dat.append(stemmer.stem(i))
+print(stemmer.stem("this is not a very appriated"))
+bag_of_words=vector.transform(dat)
